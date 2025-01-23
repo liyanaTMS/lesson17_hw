4204edcc55144ffefb3cfbc92f24122c0814280e
liana lesnikovich <liyana.lesnikovich.run4fun@gmail.com>
2025-01-23 09:35:45 +0300

    homework19 (as homework17 continued), test_calculator

--- test/test_app.py
+++ test/test_app.py
@@ -1,39 +1,59 @@
 import pytest
 from app.app import add, subtract, multiply, divide
 
-
-def test_add():
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        (2,3,5),
+        (-1,1,0),
+        (0,0,0)
+    ]
+)
+def test_add(a,b,expected):
     """Тестирует функцию сложения."""
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+    assert add(a, b) == expected
 
 
-def test_divide():
+@pytest.mark.parametrize(
+    "a,b,expect",
+    [
+        (10,2,5),
+        (9,3,3)
+    ]
+)
+def test_divide(a,b,expect):
     """Тестирует функцию деления."""
-    assert divide(10, 2) == 5
-    assert divide(9, 3) == 3
-
+    assert divide(a, b) == expect
 
 
 def test_divide_msg():
-    """
-    Тестирует вызов сообщения ValueError, если второй аргумент (b) равен нулю.
-    """
+    """Тестирует вызов сообщения ValueError, если второй аргумент (b) равен нулю."""
     with pytest.raises(ValueError) as excinfo:
         divide(1, 0)
     assert str(excinfo.value) == "Деление на ноль невозможно"
 
 
-def test_subtract():
+@pytest.mark.parametrize(
+    "a,b,expect",
+    [
+        (6,4,2),
+        (-1,8,-9),
+        (0,3,-3)
+    ]
+)
+def test_subtract(a,b,expect):
     """Тестирует функцию вычитания."""
-    assert subtract(6, 4) == 2
-    assert subtract(-1, 8) == -9
-    assert subtract(0, 3) == -3
+    assert subtract(a, b) == expect
 
 
-def test_multiply():
+@pytest.mark.parametrize(
+    "a,b,expect",
+    [
+        (3,5,15),
+        (8,-2,-16),
+        (1,0,0)
+    ]
+)
+def test_multiply(a,b,expect):
     """Тестирует функцию умножения."""
-    assert multiply(3, 5) == 15
-    assert multiply(8, -2) == -16
-    assert multiply(1, 0) == 0
+    assert multiply(a, b) == expect
